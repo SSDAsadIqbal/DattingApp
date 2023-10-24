43f7a8ddf024438af7395de4bc1a19c0800fbb79
Your Name <you@example.com>
2023-10-24 14:23:09 +0500

    end of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -9,14 +10,21 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent {
   model:any ={};
 
-  constructor(private accountService:AccountService){}
+  constructor(
+    private accountService:AccountService,
+    private toastr:ToastrService
+    ){}
 
   register(){
     this.accountService.register(this.model).subscribe({
       next: () =>{
         this.cancel();
       },
-      error:error => console.log(error)
+      error:error => {
+        this.toastr.error(error.error);
+        console.log(error);
+      }
+        
     })
   }
 
