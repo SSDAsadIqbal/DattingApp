ebb744c805586320cd17c49e0ebafa2bb16548a3
Your Name <you@example.com>
2023-11-02 17:35:03 +0500

    pre-deployment

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 builder.Services.AddIdentityServices(builder.Configuration);
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,9 +32,13 @@ app.UseCors(builder => builder
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 
 using var scope = app.Services.CreateScope();
@@ -44,7 +49,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await context .Database.ExecuteSqlRawAsync("DELETE FROM [Connections]"); 
+    await Seed.ClearConnections(context);
     await Seed.SeedUsers(userManager, roleManager);
     
 }
